b63910d97ca0b28a40a5a4878891c3a0140625b1
sana21222 <sana.alm@icloud.com>
2025-12-04 11:39:49 +0100

    ConfigurationOption.h

---
+++ Jeu_de_la_vie/ConfigurationOption.h
@@ -0,0 +1,19 @@
+#pragma once 
+#include "ConfigBase.h"
+#include <string>
+
+namespace JeuVie {
+
+class ConfigurationOption : public ConfigBase {
+private:
+    std::string option;
+
+    void chargerGlider(Population& population);
+    void chargerBlinker(Population& population);
+
+public:
+    ConfigurationOption(const std::string& option);
+    bool charger(Population& population) override;
+};
+
+}

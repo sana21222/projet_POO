e593d9a8eca519ef2c95cdc7d54134af07cc0088
ilyes800 <ililfarhat2006@gmail.com>
2025-12-04 11:15:09 +0100

    Create ConfigurationOption.cpp

---
+++ ConfigurationOption.cpp
@@ -0,0 +1,35 @@
+#include "ConfigurationOption.h"
+
+namespace JeuVie {
+
+ConfigurationOption::ConfigurationOption(const std::string& option) 
+    : option(option) {}
+
+bool ConfigurationOption::charger(Population& population) {
+    if (option == "glider") {
+        chargerGlider(population);
+        return true;
+    } else if (option == "blinker") {
+        chargerBlinker(population);
+        return true;
+    }
+    return false;
+}
+
+void ConfigurationOption::chargerGlider(Population& population) {
+    // Pattern Glider
+    population.setCellule(1, 0, true);
+    population.setCellule(2, 1, true);
+    population.setCellule(0, 2, true);
+    population.setCellule(1, 2, true);
+    population.setCellule(2, 2, true);
+}
+
+void ConfigurationOption::chargerBlinker(Population& population) {
+    // Pattern Blinker (oscillateur période 2)
+    population.setCellule(1, 1, true);
+    population.setCellule(2, 1, true);
+    population.setCellule(3, 1, true);
+}
+
+}

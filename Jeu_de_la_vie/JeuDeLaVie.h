cfe61ccb2b91d330ae120a9104251ef8971182cd
sana21222 <sana.alm@icloud.com>
2025-12-04 11:31:38 +0100

    JeuDeLaVie.h

---
+++ Jeu_de_la_vie/JeuDeLaVie.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Population.h"
+#include "ConfigBase.h"
+
+namespace JeuVie {
+
+class JeuDeLaVie {
+private:
+    Population* population;
+    ConfigBase* config;
+
+public:
+    JeuDeLaVie();
+    ~JeuDeLaVie();
+
+    void initialiser(ConfigBase* config);
+    void executer(int nbGenerations);
+    void afficher() const;
+};
+
+}
+

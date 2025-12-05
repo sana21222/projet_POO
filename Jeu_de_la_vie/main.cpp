9e23a7cba7676344424e2dca095751896e478a8d
sana21222 <sana.alm@icloud.com>
2025-12-05 09:23:12 +0100

    main.cpp

--- Jeu_de_la_vie/main.cpp
+++ Jeu_de_la_vie/main.cpp
@@ -3,27 +3,20 @@
 #include "ConfigurationOption.h"
 #include <iostream>
 #include <map>
-
 using namespace JeuVie;
-
 int main() {
     // Utilisation de map (STL)
     std::map<std::string, ConfigBase*> configurations;
     configurations["glider"] = new ConfigurationOption("glider");
     configurations["blinker"] = new ConfigurationOption("blinker");
-
     JeuDeLaVie jeu;
-
     std::cout << "Choisissez une configuration:" << std::endl;
     std::cout << "1. Glider" << std::endl;
     std::cout << "2. Blinker" << std::endl;
     std::cout << "3. Fichier personnalisé" << std::endl;
-
     int choix;
     std::cin >> choix;
-
     ConfigBase* config = nullptr;
-
     switch(choix) {
         case 1:
             config = configurations["glider"];
@@ -32,20 +25,24 @@ int main() {
             config = configurations["blinker"];
             break;
         case 3:
+        case 3: {
             std::cout << "Nom du fichier: ";
             std::string nomFichier;
             std::cin >> nomFichier;
             config = new ConfigurationFichier(nomFichier);
             break;
+        }
+        default:
+            std::cout << "Choix invalide, utilisation du glider par défaut" << std::endl;
+            config = configurations["glider"];
+            break;
     }
 
     jeu.initialiser(config);
     jeu.executer(20);
-
     // Nettoyage
     for (auto& pair : configurations) {
         delete pair.second;
     }
-
     return 0;
 }

4fe1e1ec93a6f04ad294de9e7f890969b8ae1c5f
sana21222 <sana.alm@icloud.com>
2025-12-05 08:43:40 +0100

    Etat.h

--- Jeu_de_la_vie/Etat.h
+++ Jeu_de_la_vie/Etat.h
@@ -1,13 +1,13 @@
-#pragma once
-
 namespace JeuVie {
 	class Etat {
 	public: 
 		virtual bool estVivante(int x, int y) const = 0;
 		virtual int getLargeur() const = 0;
 		virtual int getHauteur() const = 0;
+		virtual bool estVivante() const = 0;
+		virtual Etat* evoluer(int nbVoisinsVivants) const = 0;
+		virtual void afficher() const = 0;
+		virtual Etat* clone() const = 0;
 		virtual ~Etat() {}
 	};
 }
-
-// pour etat on a qu'un .h car c'est une classe abstraite

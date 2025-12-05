adc492126bc3ed2b22cfeea284b7d57e5c1ff272
ilyes800 <ililfarhat2006@gmail.com>
2025-12-05 14:08:17 +0100

    Update ConfigurationOption.cpp

--- Jeu_de_la_vie/ConfigurationOption.cpp
+++ Jeu_de_la_vie/ConfigurationOption.cpp
@@ -2,34 +2,36 @@
 
 namespace JeuVie {
 
-ConfigurationOption::ConfigurationOption(const std::string& option) 
-    : option(option) {}
-
-bool ConfigurationOption::charger(Population& population) {
-    if (option == "glider") {
-        chargerGlider(population);
-        return true;
-    } else if (option == "blinker") {
-        chargerBlinker(population);
-        return true;
+    ConfigurationOption::ConfigurationOption(const std::string& option)
+        : option(option) {
     }
-    return false;
-}
 
-void ConfigurationOption::chargerGlider(Population& population) {
-    // Pattern Glider
-    population.setCellule(1, 0, true);
-    population.setCellule(2, 1, true);
-    population.setCellule(0, 2, true);
-    population.setCellule(1, 2, true);
-    population.setCellule(2, 2, true);
-}
+    bool ConfigurationOption::charger(Population& population) {
+        if (option == "glider") {
+            chargerGlider(population);
+            return true;
+        }
+        else if (option == "blinker") {
+            chargerBlinker(population);
+            return true;
+        }
+        return false;
+    }
 
-void ConfigurationOption::chargerBlinker(Population& population) {
-    // Pattern Blinker (oscillateur période 2)
-    population.setCellule(1, 1, true);
-    population.setCellule(2, 1, true);
-    population.setCellule(3, 1, true);
-}
+    void ConfigurationOption::chargerGlider(Population& population) {
+        // Pattern Glider
+        population.setCellule(1, 0, true);
+        population.setCellule(2, 1, true);
+        population.setCellule(0, 2, true);
+        population.setCellule(1, 2, true);
+        population.setCellule(2, 2, true);
+    }
+
+    void ConfigurationOption::chargerBlinker(Population& population) {
+        // Pattern Blinker (oscillateur période 2)
+        population.setCellule(1, 1, true);
+        population.setCellule(2, 1, true);
+        population.setCellule(3, 1, true);
+    }
 
 }

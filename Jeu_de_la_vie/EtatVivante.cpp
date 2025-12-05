3995187999102e1ea8993e0b272e173032bd2714
ilyes800 <ililfarhat2006@gmail.com>
2025-12-05 14:11:58 +0100

    Update EtatVivante.cpp

--- Jeu_de_la_vie/EtatVivante.cpp
+++ Jeu_de_la_vie/EtatVivante.cpp
@@ -1,24 +1,26 @@
+#include "EtatVivante.h"
+#include "EtatMorte.h"
 #include <iostream>
 
 namespace JeuVie {
-	bool EtatVivante::estVivante() {
 	bool EtatVivante::estVivante() const {
 		return true;
 	}
-	Etat* evoluer(int nbVoisins) {
+
 	Etat* EtatVivante::evoluer(int nbVoisinsVivants) const {
-		if (nbVoisins == 2 || nbVoisins == 3) {
+		if (nbVoisinsVivants == 2 || nbVoisinsVivants == 3) {
 			return new EtatVivante();
 		}
 		else {
 			return new EtatMorte();
 		}
 	}
-	void EtatVivante::afficher() {
+
 	void EtatVivante::afficher() const {
 		std::cout << "X";
 	}
+
 	Etat* EtatVivante::clone() const {
 		return new EtatVivante();
 	}
-} 
+}

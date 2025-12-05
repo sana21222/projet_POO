81a88b4c43f2fe708dc3cb9359b38371a987b3bb
sana21222 <sana.alm@icloud.com>
2025-12-05 09:08:13 +0100

    EtatMorte.h

--- Jeu_de_la_vie/EtatMorte.h
+++ Jeu_de_la_vie/EtatMorte.h
@@ -1,10 +1,12 @@
-#pragma once
 #include "Etat.h"
 
 namespace JeuVie {
 	class EtatMort : public Etat {
+	class EtatMorte : public Etat {
 	public:
 		bool estVivant() const override;
 		Etat* evoluer(int nbVoisinsVivants) const override;
+		void afficher() const override;
+		Etat* clone() const override;
 	};
 }

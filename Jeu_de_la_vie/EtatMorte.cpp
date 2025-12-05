9e3e0f29ee8f1e0e0c0982445070489432439333
ilyes800 <ililfarhat2006@gmail.com>
2025-12-05 14:10:11 +0100

    Update EtatMorte.cpp

--- Jeu_de_la_vie/EtatMorte.cpp
+++ Jeu_de_la_vie/EtatMorte.cpp
@@ -3,10 +3,10 @@
 #include <iostream>
 
 namespace JeuVie {
-	bool EtatMorte::estVivante() {
 	bool EtatMorte::estVivante() const {
 		return false;
 	}
+
 	Etat* EtatMorte::evoluer(int nbVoisinsVivants) const {
 		if (nbVoisinsVivants == 3) {
 			return new EtatVivante();
@@ -15,9 +15,11 @@ namespace JeuVie {
 			return new EtatMorte();
 		}
 	}
+
 	void EtatMorte::afficher() const {
 		std::cout << ".";
 	}
+
 	Etat* EtatMorte::clone() const {
 		return new EtatMorte();
 	}

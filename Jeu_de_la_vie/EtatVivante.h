b657decafeba59b2f26d8517d2448c6428f97aa8
ilyes800 <ililfarhat2006@gmail.com>
2025-12-05 08:59:10 +0100

    Update EtatVivante.h

--- Jeu_de_la_vie/EtatVivante.h
+++ Jeu_de_la_vie/EtatVivante.h
@@ -2,9 +2,11 @@
 #include "etat.h"
 
 namespace JeuVie {
-	class EtatVivant : public Etat {
+	class EtatVivante : public Etat {
 	public:
 		bool estVivant() const override;
 		Etat* evoluer(int nbVoisinsVivants) const override;
+		void afficher() const override;
+		Etat* clone() const override;
 	};
 } 

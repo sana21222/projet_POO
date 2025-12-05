0422325d340660addba03b78f7d8671acc0e5ef8
ilyes800 <ililfarhat2006@gmail.com>
2025-12-05 09:14:12 +0100

    Update GridBase.h

--- Jeu_de_la_vie/GridBase.h
+++ Jeu_de_la_vie/GridBase.h
@@ -10,7 +10,6 @@ namespace JeuVie {
 	virtual ~GridBase() = default;
 	int getLargeur() const;
 	int getHauteur() const;
-	void afficher() const;
-	void prochaineGeneration();
+	virtual void afficher() const = 0;
 	};
 }

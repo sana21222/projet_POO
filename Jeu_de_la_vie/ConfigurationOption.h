569c3a9e08b203526192d0022aef9207b2738158
ilyes800 <ililfarhat2006@gmail.com>
2025-12-05 14:09:03 +0100

    Update ConfigurationOption.h

--- Jeu_de_la_vie/ConfigurationOption.h
+++ Jeu_de_la_vie/ConfigurationOption.h
@@ -4,16 +4,16 @@
 
 namespace JeuVie {
 
-class ConfigurationOption : public ConfigBase {
-private:
-    std::string option;
+    class ConfigurationOption : public ConfigBase {
+    private:
+        std::string option;
 
-    void chargerGlider(Population& population);
-    void chargerBlinker(Population& population);
+        void chargerGlider(Population& population);
+        void chargerBlinker(Population& population);
 
-public:
-    ConfigurationOption(const std::string& option);
-    bool charger(Population& population) override;
-};
+    public:
+        ConfigurationOption(const std::string& option);
+        bool charger(Population& population) override;
+    };
 
 }
